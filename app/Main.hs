bb461d4f0adb9f26dd4585188209146827a28980
chris-bacon <christopher.bacon@bbc.co.uk>
2018-05-04 17:44:58 +0100

    Fixed centre correctly so that stars move out from centre

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Graphics.Gloss
 import System.Random
+import System.Environment
 
 data Star = Star 
     { x :: Float
@@ -16,7 +17,7 @@ width = 1024
 height = 800
 
 window :: Display
-window = InWindow "My window" (1024, 800) (10, 10)
+window = InWindow "Starfield" (1024, 800) (10, 10)
 
 background :: Color
 background = black
@@ -24,11 +25,11 @@ background = black
 renderStar :: Star -> Picture
 renderStar star = translate (x star) (y star) $ color white $ circleSolid 1
 
-createStar :: IO Star
-createStar = do
-    x <- randomRIO ((-width/2), width/2)
-    y <- randomRIO ((-height/2), height/2)
-    z <- randomRIO (1, 3)
+createStar :: Float -> IO Star
+createStar maxSpeed = do
+    x <- randomRIO ((-width / 2), width / 2)
+    y <- randomRIO ((-height / 2), height / 2)
+    z <- randomRIO (1, maxSpeed)
     pure $ Star x y z
 
 render :: Starfield -> Picture
@@ -37,22 +38,25 @@ render (Starfield a) = pictures $ fmap renderStar a
 moveStar :: Float -> Star -> Star
 moveStar time star = Star (x star + xrate) (y star + yrate) (z star)
     where
-        xrate = (x star) - centerX * (z star) * time
-        yrate = (y star) - centerY * (z star) * time
-        centerX = width / 2
-        centerY = height / 2
+        xrate = ((x star) - centerX) * (z star) * time / 10 -- ^ The further from the centre the faster it gets
+        yrate = ((y star) - centerY) * (z star) * time / 10 -- ^ Ditto
+        centerX = 0
+        centerY = 0
 
 moveStars :: Float -> Starfield -> Starfield
 moveStars time (Starfield a) = Starfield $ fmap (moveStar time) a
 
-initialState :: Int -> IO Starfield
-initialState numStars = do
-    stars <- sequence $ take numStars $ repeat $ createStar
+initialState :: Int -> Float -> IO Starfield
+initialState numStars maxSpeed = do
+    stars <- sequence $ take numStars $ repeat $ createStar maxSpeed
     pure $ Starfield stars
 
 main :: IO ()
 main = do
-    stars <- initialState 50
+    args <- getArgs
+    let numStars = 5000
+        maxSpeed = read (args !! 0) :: Float
+    stars <- initialState numStars maxSpeed
     animate window background (frame stars)
         where
             frame :: Starfield -> Float -> Picture
