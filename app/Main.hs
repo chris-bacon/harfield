02f1d3c36e904ce96e637ca89a4f4e13f9d6a9dd
chris-bacon <christopher.bacon@bbc.co.uk>
2018-05-07 11:40:01 +0100

    Moved from animate to animateIO

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Animate
 import System.Random
 import System.Environment
 
@@ -12,6 +13,8 @@ data Star = Star
 
 data Starfield = Starfield [Star]
 
+newtype MaxSpeed = MaxSpeed Float
+
 width, height :: Float 
 width = 1024
 height = 800
@@ -22,43 +25,63 @@ window = InWindow "Starfield" (1024, 800) (10, 10)
 background :: Color
 background = black
 
-renderStar :: Star -> Picture
-renderStar star = translate (x star) (y star) $ color white $ circleSolid 1
+renderStar :: Star -> IO Picture
+renderStar star = pure $ translate (x star) (y star) $ color white $ circleSolid 1
 
-createStar :: Float -> IO Star
-createStar maxSpeed = do
+createStar :: MaxSpeed -> IO Star
+createStar (MaxSpeed maxSpeed) = do
     x <- randomRIO ((-width / 2), width / 2)
     y <- randomRIO ((-height / 2), height / 2)
     z <- randomRIO (1, maxSpeed)
     pure $ Star x y z
 
-render :: Starfield -> Picture
-render (Starfield a) = pictures $ fmap renderStar a
+render :: Starfield -> IO Picture
+render (Starfield a) = do
+    stars <- mapM renderStar a
+    pure $ pictures stars
 
-moveStar :: Float -> Star -> Star
-moveStar time star = Star (x star + xrate) (y star + yrate) (z star)
-    where
-        xrate = ((x star) - centerX) * (z star) * time / 10 -- ^ The further from the centre the faster it gets
-        yrate = ((y star) - centerY) * (z star) * time / 10 -- ^ Ditto
-        centerX = 0
-        centerY = 0
+offScreen :: Star -> Bool
+offScreen star
+  | x star > (width / 2) - 10 = True
+  | y star > height / 2 = True
+  | x star < (-(width / 2)) = True
+  | y star < (-(height / 2)) = True
+  | otherwise = False
 
-moveStars :: Float -> Starfield -> Starfield
-moveStars time (Starfield a) = Starfield $ fmap (moveStar time) a
+moveStar :: Float -> Star -> IO Star
+moveStar time star = 
+    if offScreen star
+        then (createStar (MaxSpeed 20)) 
+        else pure $ Star (x star + xrate) (y star + yrate) (z star) 
+            where
+                xrate = ((x star) - centerX) * (z star) * time / 10 -- ^ The further from the centre the faster it gets
+                yrate = ((y star) - centerY) * (z star) * time / 10 -- ^ Ditto
+                centerX = 0
+                centerY = 0
 
-initialState :: Int -> Float -> IO Starfield
+moveStars :: Float -> Starfield -> IO Starfield
+moveStars time (Starfield a) = do
+    newStars <- mapM (moveStar time) $ a
+    pure $ Starfield newStars
+
+initialState :: Int -> MaxSpeed -> IO Starfield
 initialState numStars maxSpeed = do
     stars <- sequence $ take numStars $ repeat $ createStar maxSpeed
     pure $ Starfield stars
+
+--f :: Controller -> IO ()
+f = const $ return ()
 
 main :: IO ()
 main = do
     args <- getArgs
     let numStars = 5000
-        maxSpeed = read (args !! 0) :: Float
+        maxSpeed = MaxSpeed $ read (args !! 0)
     stars <- initialState numStars maxSpeed
-    animate window background (frame stars)
+    animateIO window background (frame stars) f
         where
-            frame :: Starfield -> Float -> Picture
-            frame stars seconds = render $ moveStars seconds stars
+            frame :: Starfield -> Float -> IO Picture
+            frame stars seconds = do
+                stars <- moveStars seconds stars
+                render stars
 
